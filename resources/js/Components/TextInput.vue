48eb187a2adb43cf544dccd7a29b4a4193e9137e
Stas <stanislav.pogorskii@gmail.com>
2024-05-31 17:56:59 +0400

    feat: breeze

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts">
-import { onMounted, ref } from 'vue';
+<script lang="ts" setup>
+import {onMounted, ref} from 'vue';
 
-const model = defineModel<string>({ required: true });
+const model = defineModel<string>({required: true});
 
 const input = ref<HTMLInputElement | null>(null);
 
@@ -11,13 +11,12 @@ onMounted(() => {
     }
 });
 
-defineExpose({ focus: () => input.value?.focus() });
+defineExpose({focus: () => input.value?.focus()});
 </script>
-
 <template>
     <input
-        class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-        v-model="model"
         ref="input"
+        v-model="model"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     />
 </template>
