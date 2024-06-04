ad8ea7d269e7968c9fbf7604a20deea38c491faa
Stas <stanislav.pogorskii@gmail.com>
2024-06-04 18:34:02 +0400

    refactor: igdb macros

--- app/Actions/Games/IGDB/FetchGamesFromIGDBAction.php
+++ app/Actions/Games/IGDB/FetchGamesFromIGDBAction.php
@@ -4,6 +4,8 @@ namespace App\Actions\Games\IGDB;
 
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\Pool;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
 class FetchGamesFromIGDBAction
@@ -13,39 +15,47 @@ class FetchGamesFromIGDBAction
      */
     public static function execute(int $iterations = 1, string $sortingRule = "updated_at desc"): array
     {
-        $fields = implode(', ', [
-            '*',
-            'age_ratings.*',
-            'age_ratings.content_descriptions.*',
-            'alternative_names.*',
-            'cover.*',
-            'game_localizations.*',
-            'external_games.*',
-            'language_supports.*',
-            'release_dates.*',
-            'screenshots.*',
-            'videos.*',
-            'websites.*',
-            'collection.*',
-            'collections.*',
-            'franchise.*',
-            'franchises.*',
-            'game_engines.*',
-        ]);
+        try {
+            $fields = implode(', ', [
+                '*',
+                'age_ratings.*',
+                'age_ratings.content_descriptions.*',
+                'alternative_names.*',
+                'cover.*',
+                'game_localizations.*',
+                'external_games.*',
+                'language_supports.*',
+                'release_dates.*',
+                'screenshots.*',
+                'videos.*',
+                'websites.*',
+                'collection.*',
+                'collections.*',
+                'franchise.*',
+                'franchises.*',
+                'game_engines.*',
+            ]);
 
-        $responses = Http::pool(function (Pool $pool) use ($iterations, $fields, $sortingRule) {
-            for ($i = 0; $i < $iterations; $i++) {
-                $offsetValue = $i * 500;
-                $body        = "fields {$fields}; where themes != (42); limit 500; offset {$offsetValue}; sort {$sortingRule};";
-                $pool->as($i)->igdb()->withBody($body)->post("games");
+            $responses = Http::pool(function (Pool $pool) use ($iterations, $fields, $sortingRule) {
+                for ($i = 0; $i < $iterations; $i++) {
+                    $offsetValue = $i * 500;
+                    $body        = "fields {$fields}; where themes != (42); limit 500; offset {$offsetValue}; sort {$sortingRule};";
+                    $pool->as($i)->igdb()->withBody($body)->post("games");
+                }
+            });
+
+            $fetchedGames = [];
+            foreach ($responses as $response) {
+                if (gettype($response) === 'object' && $response instanceof Response && $response->successful()) {
+                    $fetchedGames = array_merge($fetchedGames, $response->json());
+                } else {
+                    throw $response;
+                }
             }
-        });
 
-        $fetchedGames = [];
-        collect($responses)->map(function ($response) use (&$fetchedGames) {
-            $fetchedGames = array_merge($fetchedGames, $response->json());
-        });
-
-        return $fetchedGames;
+            return $fetchedGames;
+        } catch (RequestException $e) {
+            throw new ConnectionException('An error occurred while fetching games fromdfsfsfsf IGDB: '.$e->getMessage());
+        }
     }
 }
