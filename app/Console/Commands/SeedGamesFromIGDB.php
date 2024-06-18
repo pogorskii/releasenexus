2b6e6422a021e8edfa9b65d794d7b67c91eb40c1
Stas <stanislav.pogorskii@gmail.com>
2024-06-18 23:21:14 +0400

    feat: fetch age rating descriptions

---
+++ app/Console/Commands/SeedGamesFromIGDB.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SeedGamesFromIGDBJob;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Bus;
+
+class SeedGamesFromIGDB extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'igdb:seed-games';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetches games from IGDB and adds them to the database.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        try {
+            $this->info('Seeding all games from IGDB...');
+
+            $jobs = [];
+            for ($i = 0; $i < 150; $i++) {
+                $jobs[] = new SeedGamesFromIGDBJob($i);
+            }
+
+            $this->withProgressBar($jobs, fn($job) => Bus::dispatch($job));
+            $this->newLine();
+            $this->info('Finished seeding all games from IGDB.');
+        } catch (\Exception|\Throwable $e) {
+            $this->error('An error occurred while seeding all games from IGDB: '.$e->getMessage());
+        }
+    }
+}
