0531c8f4b824ba2902e9a11c27aed7ef96a60281
Stas <stanislav.pogorskii@gmail.com>
2024-06-24 21:29:52 +0400

    feat: game cards

--- resources/js/Pages/Games/Calendar.vue
+++ resources/js/Pages/Games/Calendar.vue
@@ -69,28 +69,54 @@ const formatDate = (date: string): string => {
 </script>
 <template>
     <div class="bg-black w-full min-w-full">
-        <h1 class="text-2xl font-semibold">Games releasing in {{ monthsMap[Number(month)] }} {{ year }}</h1>
-        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-1">
+        <div class="flex gap-10">
+            <InertiaLink
+                :href="`/games/calendar/${year}/${Number(month) - 1}`"
+                class="px-4 py-2 block bg-purple-400 w-full font-semibold text-center">Previous month
+            </InertiaLink>
+            <h1 class="text-2xl font-semibold whitespace-nowrap">Games releasing in {{ monthsMap[Number(month)] }} {{ year }}</h1>
+            <InertiaLink
+                :href="`/games/calendar/${year}/${Number(month) + 1}`"
+                class="px-4 py-2 block bg-purple-400 w-full font-semibold text-center">Next month
+            </InertiaLink>
+        </div>
+        <div class="grid grid-cols-1 gap-4">
             <div v-for="day in datedReleases"
                  class="bg-black p-6 flex gap-6">
-                <h2 class="text-2xl font-semibold whitespace-nowrap">{{ formatDate(day.date) }}</h2>
-                <div class="grow grid grid-cols-3 gap-2">
+                <div>
+                    <h2 class="text-2xl font-semibold whitespace-nowrap border-b pb-2">{{ formatDate(day.date) }}</h2>
+                </div>
+                <div class="grow grid grid-cols-3 2xl:grid-cols-4 gap-6">
                     <div v-for="release in day.releases"
-                         class="bg-gray-800 overflow-hidden shadow sm:rounded-lg p-4 min-h-[15rem] flex flex-col justify-between">
-                        <img
-                            :src="'https://images.igdb.com/igdb/image/upload/t_original/' + release.game.covers[0]?.g_image_id + '.jpg'"
-                            alt="Cover image" class="w-full h-fit object-cover"/>
-                        <h3 class="text-xl font-semibold mb-2">{{ release.game.name }} ({{ gameTypeMap[release.game.category] }})</h3>
-                        <div class="w-full h-[2px] bg-purple-400 mb-2"></div>
-                        <div class="flex gap-2 mb-auto">
-                            <div v-for="releaseDate in release.release_dates.filter(r => r.region == 8)"
-                                 class="text-sm">
-                                {{ releaseDate.platform.abbreviation ?? releaseDate.platform.name }}
+                         class="bg-gray-800 overflow-hidden shadow sm:rounded-lg aspect-[3/5] flex flex-col justify-between">
+                        <div class="relative overflow-hidden">
+                            <img
+                                :src="'https://images.igdb.com/igdb/image/upload/t_original/' + release.game.covers[0]?.g_image_id + '.jpg'"
+                                alt="Cover image"
+                                class="duration-200 ease-in-out hover:scale-105 object-cover w-full"
+                                :width="release.game.covers[0]?.image.width || 600"
+                                :height="release.game.covers[0]?.image.height || 900"
+                            />
+                            <div
+                                class="absolute left-2 top-2 z-10 text-black uppercase text-xs bg-white p-2 font-bold rounded-lg bg-opacity-75">{{ gameTypeMap[release.game.category] }}
                             </div>
                         </div>
-                        <!--                        {{ release.game.covers[0] }}-->
+                        <div class="flex grow flex-col p-6 pb-3">
+                            <h3 class="mb-4 font-semibold text-xl leading-tight">
+                                <InertiaLink
+                                    class="hover:text-primary hover:underline hover:decoration-solid hover:underline-offset-4">{{ release.game.name }}
+                                </InertiaLink>
+                            </h3>
+                            <div class="w-full h-[2px] bg-purple-400 mb-2"></div>
+                            <div class="mb-auto inline-flex flex-wrap gap-2 self-start">
+                                <div v-for="releaseDate in release.release_dates.filter(r => r.region == 8)"
+                                     class="text-sm">
+                                    {{ releaseDate.platform.abbreviation ?? releaseDate.platform.name }}
+                                </div>
+                            </div>
+                        </div>
                         <InertiaLink
-                            class="px-4 py-2 block bg-purple-400 w-full rounded-lg font-semibold">More info &rarr;
+                            class="px-4 py-2 block bg-purple-400 w-full font-semibold text-center">More info &rarr;
                         </InertiaLink>
                     </div>
                 </div>
